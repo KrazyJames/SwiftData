053a17c16d35936c19fc6def33860f4a770159fe
Jaime Escobar Martinez <31583280+KrazyJames@users.noreply.github.com>
2023-12-05 10:34:21 -0700

    Getting Started

---
+++ Movies/Movies/MoviesApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoviesApp.swift
+//  Movies
+//
+//  Created by Jaime Escobar on 05/12/23.
+//
+
+import SwiftUI
+
+@main
+struct MoviesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
