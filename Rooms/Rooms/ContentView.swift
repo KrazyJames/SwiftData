6d309b20b26d60dc5da353667871edee4616940b
Jaime Escobar Martinez <31583280+KrazyJames@users.noreply.github.com>
2023-12-20 10:20:08 -0700

    UI for Rooms
    Registered the ValueTransformer class

--- Rooms/Rooms/ContentView.swift
+++ Rooms/Rooms/ContentView.swift
@@ -6,19 +6,80 @@
 //
 
 import SwiftUI
+import SwiftData
 
 struct ContentView: View {
+    @Environment(\.modelContext) private var modelContext
+    
+    @State private var name: String = ""
+    @State private var color: Color = .clear
+
+    @Query private var rooms: [Room]
+
+    private var isDisabled: Bool {
+        name.trimmingCharacters(in: .whitespaces).isEmpty || color == .clear
+    }
+
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationStack {
+            Form {
+                Section("New room") {
+                    TextField("Room name", text: $name)
+                        .textFieldStyle(.roundedBorder)
+                    ColorPicker("Pick your color", selection: $color, supportsOpacity: false)
+                    VStack(alignment: .center) {
+                        Button("Save") {
+                            save()
+                        }
+                        .buttonStyle(.borderedProminent)
+                        .disabled(isDisabled)
+                    }
+                    .frame(maxWidth: .infinity)
+                }
+                .listRowSeparator(.hidden)
+
+                Section("Saved rooms") {
+                    List {
+                        ForEach(rooms) { room in
+                            RoomRowView(room: room)
+                        }
+                        .onDelete(perform: delete)
+                    }
+                }
+            }
+            .navigationTitle("Rooms")
         }
-        .padding()
+    }
+
+    private func save() {
+        let room = Room(name: name, color: UIColor(color))
+        modelContext.insert(room)
+        name = .init()
+        color = .clear
+    }
+
+    private func delete(at indexSet: IndexSet) {
+        for index in indexSet {
+            modelContext.delete(rooms[index])
+        }
+    }
+}
+
+struct RoomRowView: View {
+    let room: Room
+    var body: some View {
+        HStack {
+            Text(room.name)
+            Spacer()
+            Rectangle()
+                .fill(Color(uiColor: room.color))
+                .frame(width: 48, height: 48)
+                .clipShape(.rect(cornerRadius: 8))
+        }
     }
 }
 
 #Preview {
     ContentView()
+        .modelContainer(DataController.previewContainer)
 }
--- Rooms/Rooms/RoomsApp.swift
+++ Rooms/Rooms/RoomsApp.swift
@@ -6,12 +6,48 @@
 //
 
 import SwiftUI
+import SwiftData
 
 @main
 struct RoomsApp: App {
+
+    // MARK: - Register Value Transformer
+    init() {
+        ValueTransformer.setValueTransformer(
+            UIColorTransformer(),
+            forName: .init("UIColorTransformer")
+        )
+    }
+
     var body: some Scene {
         WindowGroup {
             ContentView()
+                .modelContainer(
+                    for: [Room.self]
+                )
         }
+    }
+}
+
+@MainActor
+final class DataController {
+    static var previewContainer: ModelContainer {
+        do {
+            let container = try ModelContainer(
+                for: Room.self,
+                configurations: ModelConfiguration(
+                    isStoredInMemoryOnly: true
+                )
+            )
+            let room = Room(
+                name: "Kitchen",
+                color: .red
+            )
+            container.mainContext.insert(room)
+            return container
+        } catch {
+            fatalError(error.localizedDescription)
+        }
+
     }
 }
